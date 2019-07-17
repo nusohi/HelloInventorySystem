cb801b5b4dae5c93524fa84bf1194fd3bf7d2e62
nuso <nelohjb@hotmail.com>
2019-07-17 16:04:43 +0800

    Prefab和Scene

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    public ToolTip ToolTip;
+    public string ToolTipText;
+
+    private Button button;
+
+    private void Start() {
+        if (ToolTip == null) {
+            ToolTip = GameObject.Find("ToolTip").GetComponent<ToolTip>();
+        }
+
+        button = GetComponent<Button>();
+        //button.onClick.AddListener(ShowToolTip);
+        //button.onClick.AddListener(ShowChest);
+        button.onClick.AddListener(GetRandomItem);
+    }
+
+    public void ShowToolTip() {
+        Debug.Log("TEST - Show Tool Tip Test!" + " (test: '" + ToolTipText + "' )");
+        ToolTip.Show(ToolTipText);
+    }
+
+    public void ShowChest() {
+        Chest.Instance.DisplaySwitch();
+    }
+
+    public void GetRandomItem() {
+        int ID = Random.Range(1, 18);
+        Chest.Instance.StoreItem(ID);
+    }
+}
